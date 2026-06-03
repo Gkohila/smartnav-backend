81d16f661519e1cd301bb8b4e600570ea1ef7f8b
Gkohila <kohila2024@gmail.com>
2026-06-03 12:28:25 +0530

    Initial Spring Boot Setup

---
+++ src/main/java/com/smartnav/smartnav_backend/SmartnavBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartnav.smartnav_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartnavBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartnavBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartnav/smartnav_backend/SmartnavBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartnav.smartnav_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartnavBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
