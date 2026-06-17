942d60c6fdb50f883dccd5333c4b651499fb271c
Gkohila <kohila2024@gmail.com>
2026-06-17 10:45:17 +0530

    Updated OTP authendication

--- src/main/java/com/smartnav/smartnav_backend/service/UserService.java
+++ src/main/java/com/smartnav/smartnav_backend/service/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.smartnav.smartnav_backend.security.JwtService;
 
 import com.smartnav.smartnav_backend.entity.User;
 import com.smartnav.smartnav_backend.repository.UserRepository;
